66591d6f98476311d53c82a89a0a9e1e6911ef84
pritam magadum <pritammagadum@pritams-MacBook-Pro.local>
2022-01-07 18:45:16 +0530

    added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,43 @@
-import '../styles/globals.css'
-import Layout from '../components/layout/layout'
-import Head from 'next/head'
+import { useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import '../styles/globals.css';
+import Layout from '../components/layout/layout';
+import Head from 'next/head';
+import { GTM_ID, pageview } from '../lib/gtm';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview);
+    return () => {
+      router.events.off('routeChangeComplete', pageview);
+    };
+  }, [router.events]);
   return (
     <Layout>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+      />
+      <Script
+        strategy='lazyOnload'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React, { Component } from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GTM_ID, pageview } from '../lib/gtm';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="eng">
+      <Html lang='eng'>
         <Head />
         <body>
+          <noscript>
+            <iframe
+              src='https://www.googletagmanager.com/ns.html?id=GTM-WVLBRQ4'
+              height='0'
+              width='0'
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+
           <Main />
           <NextScript />
-          <div id="notifications"></div>
+          <div id='notifications'></div>
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
